dfd0d8380c9638bb24e18b0911a1c571a36df262
Edgar <edgarbar18@icloud.com>
2026-08-03 17:18:17 -0600

    Correccioin

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,6 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from "./Pages/login";
 import Dashboard from './Pages/Dashboard';
-import Personal from './Pages/personal';
-import Nomina from './Pages/nomina';
-import CapturaDiaria from './Pages/capturaDiaria';
-import VerRegistro from './Pages/verRegistro';
-import Inventario from './Pages/Inventario';
-import Almacenes from './Pages/almacen';
-import Catalogos from './Pages/catalogos';
-import Entradas from './Pages/entradas';
-import Proveedores from './Pages/proveedores';
-import Salidas from './Pages/salidas';
-import FacturasProv from './Pages/facProv';
-import Clientes from './Pages/clientes'; // NUEVA IMPORTACIÓN PARA CLIENTES
-import FacturasCli from './Pages/facCli'; // NUEVA IMPORTACIÓN PARA FACTURAS DE CLIENTES
-
-
 
 function App() {
   // Estado inicial basado en si existe el token en el navegador
@@ -54,97 +39,6 @@ function App() {
           element={isAuth ? <Dashboard onLogout={logoutAction} /> : <Navigate to="/login" />}
         />
 
-        {/* Personal Protegido */}
-        <Route
-          path="/personal"
-          element={isAuth ? <Personal onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        {/* Nomina Protegido */}
-        <Route
-          path="/nomina"
-          element={isAuth ? <Nomina onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/captura-diaria"
-          element={isAuth ? <CapturaDiaria onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/verRegistro"
-          element={isAuth ? <VerRegistro onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/productos"
-          element={isAuth ? <Inventario onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/catalogos"
-          element={isAuth ? <Catalogos onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/entradas"
-          element={isAuth ? <Entradas onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/entradas/crear"
-          element={isAuth ? <Entradas onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/entradas/editar/:id"
-          element={isAuth ? <Entradas onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/entradas/:id"
-          element={isAuth ? <Entradas onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/almacen"
-          element={isAuth ? <Almacenes onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/proveedores"
-          element={isAuth ? <Proveedores onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/proveedores/crear"
-          element={isAuth ? <Proveedores onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/proveedores/editar/:id"
-          element={isAuth ? <Proveedores onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route
-          path="/proveedores/:id"
-          element={isAuth ? <Proveedores onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-
-        <Route
-          path="/salidas"
-          element={isAuth ? <Salidas onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route 
-          path="/facProv"
-          element={isAuth ? <FacturasProv onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route 
-          path="/clientes"
-          element={isAuth ? <Clientes onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        <Route 
-          path="/facCli"
-          element={isAuth ? <FacturasCli onLogout={logoutAction} /> : <Navigate to="/login" />}
-        />
-        {/* Comodín: Cualquier otra ruta inválida redirige al inicio */}
-        <Route path="*" element={<Navigate to={isAuth ? "/dashboard" : "/login"} />} />
-
-
-
-        {/* Aquí agregaríamos más rutas protegidas según sea necesario */}
-        <Route path="/Inventario" element={isAuth ? <Inventario onLogout={logoutAction} /> : <Navigate to="/login" />} />
         
       </Routes>
     </Router>
--- src/api.jsx
+++ src/api.jsx
@@ -3,22 +3,5 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const ENDPOINTS = {
     login: `${BASE_URL}usuarios/login/`,
-    empleados: `${BASE_URL}apiEmp/empleados/`,
-    entradas: `${BASE_URL}/apiInv/entradas/`,
-    facturas: `${BASE_URL}apiProv/facturas/`, 
-    facturasCli: `${BASE_URL}/apiCli/facturas/`, 
-    pagos: `${BASE_URL}/apiProv/pagos/`, 
-    
-    // CORRECCIÓN AQUÍ: Cambia 'sincronizar-reloj/' por 'sincronizar/'
-    sincronizarReloj: `${BASE_URL}/apiNom/reloj/sincronizar/`, 
-    movimientos: `${BASE_URL}/apiInv/movimientos/`,
-    productos: `${BASE_URL}apiInv/productos/`,
-    almacenes: `${BASE_URL}apiInv/almacenes/`,
-    conceptos: `${BASE_URL}apiInv/conceptos/`,
-    embaces: `${BASE_URL}apiInv/embaces/`,
-    tarimas: `${BASE_URL}apiInv/tarimas/`,
-    proveedores: `${BASE_URL}apiProv/proveedores/`,
-    clientes: `${BASE_URL}apiCli/clientes/`,
-    stock: `${BASE_URL}apiInv/stockActual/`,
-    empresas: `${BASE_URL}apiEmpr/empresa/principal/`,
+   
 };
